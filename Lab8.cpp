4005522c5c95c4cea4a7d87a2134691f2075bd9d
alinaast <55184585+alinaast@users.noreply.github.com>
2019-10-20 14:32:56 +0300

    Add files via upload

---
+++ Lab8.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "rus");
+	int n, m;
+	cin >> n >> m;
+	cout << n << " ";
+	if (m == 1)
+		cout << "января";
+	if (m == 2)
+			cout << "февраля";
+	if (m == 3)
+		cout << "марта";
+	if (m == 4)
+		cout << "апреля";
+	if (m == 5)
+		cout << "мая";
+	if (m == 6)
+		cout << "июня";
+	if (m == 7)
+		cout << "июля";
+	if (m == 8)
+		cout << "августа";
+	if (m == 9)
+		cout << "сентября";
+	if (m == 10)
+		cout << "октября";
+	if (m == 11)
+		cout << "ноября";
+	if (m == 12)
+		cout << "декабря";
+	return 0;
+} 
+
---
+++ Lab9.2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "rus");
+	int n, c;
+	cin >> n;
+	cin >> c;
+	if (n == 0) {
+		cout << "продолжить движение";
+	}
+	if (n == 1) {
+		cout << "поворот налево";
+	}
+	if (n == -1) {
+		cout << "поворот направо";
+	}
+	if (c == 0) {
+		cout << "" << "на север";
+	}
+	if (c == 1) {
+		cout << " " << "на запад";
+	}
+	if (c == 2) {
+		cout << " " << "на юг";
+	}
+	if (c == 3) {
+		cout << " " << "на восток";
+	}
+	return 0;
+}
